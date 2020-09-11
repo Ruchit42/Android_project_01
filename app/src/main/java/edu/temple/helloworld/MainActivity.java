06819082595f6a8ce20d11ffed91faea2ffc1d27
Ruchit Patel <tug86709@email.com>
2020-09-11 01:43:06 -0400

    The button works

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,25 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+   TextView txt ;
+   Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.myText);
+        but = (Button) findViewById(R.id.myButton);
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("You have clicked the button");
+            }
+        });
     }
 }
